3ae8033288205b1d9ab2a2f1e0a0773d5bde24cf
TC <teresa.j.campbell@gmail.com>
2021-12-23 10:58:55 -0600

    local storage working!

--- JS/calendar.js
+++ JS/calendar.js
@@ -38,17 +38,22 @@ $(document).ready(function () {
 
     var interval = setInterval(timeBlocks, 15000);
 
-    //console.log(saveAppointments);
+   
+    
 
-    $('#hour-9 .description').val(localStorage.getItem());
-    $('#hour-10 .description').val(localStorage.getItem());
-    $('#hour-11 .description').val(localStorage.getItem());
-    $('#hour-12 .description').val(localStorage.getItem());
-    $('#hour-13 .description').val(localStorage.getItem());
-    $('#hour-14 .description').val(localStorage.getItem());
-    $('#hour-15 .description').val(localStorage.getItem());
-    $('#hour-16 .description').val(localStorage.getItem());
-    $('#hour-17 .description').val(localStorage.getItem());
+    $('#hour-9 .description').val(localStorage.getItem("hour-9"));
+    $('#hour-10 .description').val(localStorage.getItem("hour-10"));
+    $('#hour-11 .description').val(localStorage.getItem("hour-11"));
+    $('#hour-12 .description').val(localStorage.getItem("hour-12"));
+    $('#hour-13 .description').val(localStorage.getItem("hour-13"));
+    $('#hour-14 .description').val(localStorage.getItem("hour-14"));
+    $('#hour-15 .description').val(localStorage.getItem("hour-15"));
+    $('#hour-16 .description').val(localStorage.getItem("hour-16"));
+    $('#hour-17 .description').val(localStorage.getItem("hour-17"));
+    
+    
+
+   
 
     $("#currentDay").text(moment().format('dddd, MMMM Do'));
 
